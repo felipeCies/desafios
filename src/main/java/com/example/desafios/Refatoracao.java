5b068f203879e0263a02551684e10be6ba525a35
felipeCies <cieslick246@gmail.com>
2023-08-07 20:04:16 -0300

    att

---
+++ src/main/java/com/example/desafios/Refatoracao.java
@@ -0,0 +1,23 @@
+package com.example.desafios;
+
+public class Refatoracao {
+    public static double main(String[] args) {
+
+    }
+
+        public static double salvaX(double x){
+            double xSalvo = x;
+            return xSalvo;
+        }
+
+    public static double salvaY(double y){
+            double ySalvo = y;
+            return ySalvo;
+        }
+
+    public static double somaXeY(double x, double y){
+            return salvaX(x) + salvaY(y);
+        }
+
+
+
